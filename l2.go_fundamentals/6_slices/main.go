c97e50e330e6f2e770becc3999bdfa5367982f5c
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-01-16 00:07:00 -0500

    modified slices, added structs folder structs

--- l2.go_fundamentals/6_slices/main.go
+++ l2.go_fundamentals/6_slices/main.go
@@ -15,4 +15,25 @@ func main() {
 	fmt.Println(animals)
 	fmt.Printf("Animals2 slice len => %d", len(animals2))
 	fmt.Println(animals2)
+	fmt.Printf("Slice length => %d,  capacity => %d\n", len(animals), cap(animals))
+
+	//declare empty slices
+	var newSlice []int
+	fmt.Printf(" initial Slice length => %d,  capacity => %d\n", len(newSlice), cap(newSlice))
+
+	newSlice = append(newSlice, 1)
+	newSlice = append(newSlice, 2)
+	newSlice = append(newSlice, 3)
+	newSlice = append(newSlice, 4)
+
+	fmt.Printf(" Slice length => %d,  capacity => %d\n", len(newSlice), cap(newSlice))
+
+	fmt.Printf("new slice => %d\n", newSlice)
+	fmt.Printf("new slice => %v\n", newSlice)
+
+	var myNewSlice2 []int
+	myNewSlice2 = make([]int, 10)
+
+	fmt.Printf(" initial Slice length => %d,  capacity => %d\n", len(myNewSlice2), cap(myNewSlice2))
+
 }
