5da7137da9a26dfc7a5d3b76bad0a5e80a2f46b7
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-02-06 16:23:51 -0500

    added channel to wait for goroutines

--- l2.go_fundamentals/12_Goroutines/main_goroutines.go
+++ l2.go_fundamentals/12_Goroutines/main_goroutines.go
@@ -8,19 +8,26 @@ import (
 func iterates(str []string) {
 	for i, value := range str {
 		fmt.Printf("[Index %d - value=%s\n]", i, value)
-		time.Sleep(1 * time.Second)
+		time.Sleep(100 * time.Millisecond)
 	}
 	fmt.Println()
 }
 
 func main() {
-
 	slc1 := []string{"hi", "there", "!"}
 	slc2 := []string{"hi", "there", "!", "you", "'re", "always", "welcome"}
-
 	iterates(slc1)
 	iterates(slc2)
 
+	waitChannel := make(chan bool)
 	go iterates(slc1)
 	go iterates(slc2)
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		waitChannel <- true
+	}()
+
+	retWait := <-waitChannel
+	fmt.Printf("RetWait : %v\n", retWait)
 }
